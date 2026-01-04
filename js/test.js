a4c4b2e6e321f287bbaa15173c0bd658f1d8a7c8
Nithish0107 <nithish.k.rajkumar@accenture.com>
2026-01-04 20:16:20 +0530

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,11 +1,3 @@
-function bad_function_name() {
-  return 1;
-}
- 
-const BadFunction = function () {
-  return 2;
-};
- 
-const another_bad_function = () => {
-  return 3;
-};
+function bad_function_name() {}
+const BadFunction = function () {}
+const another_bad_function = () => {}
