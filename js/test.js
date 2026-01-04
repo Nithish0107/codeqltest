18968a2bb11cf93ed6ef297498805c70ac734eae
Nithish0107 <nithish.k.rajkumar@accenture.com>
2026-01-05 00:03:54 +0530

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,16 +1,25 @@
-function Bad_function() {
-  return 1;
-}
+function Bad_function_name() {
+  let bad_variable_name = 10;
+  let Another_Bad_Variable = 20;
  
-const BAD_variable = 10;
- 
-class bad_class {
-  method_one() {
-    const Another_Bad = 5;
-    return Another_Bad;
+  function inner_BadFunction() {
+    let inner_bad_variable = 5;
+    return inner_bad_variable;
   }
+ 
+  let result_value = inner_BadFunction();
+  return result_value + bad_variable_name;
 }
  
-const another_bad_function = () => {
-  return 2;
-};
+function another_bad_function() {
+  var wrong_var_name = 100;
+  let CorrectVariable = 50;
+ 
+  function Nested_bad_function() {
+    let nested_wrong_name = 1;
+    return nested_wrong_name;
+  }
+ 
+  return wrong_var_name + Nested_bad_function();
+}
+ 
