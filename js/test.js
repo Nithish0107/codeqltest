1dff085d3c6342b22f6aec2f0cc68c217164614b
Nithish0107 <nithish.k.rajkumar@accenture.com>
2026-01-04 20:01:23 +0530

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,3 +1,11 @@
-function do_stuff() {
-  console.log("force violation");
+function bad_function_name() {
+  return 1;
 }
+ 
+const BadFunction = function () {
+  return 2;
+};
+ 
+const another_bad_function = () => {
+  return 3;
+};
